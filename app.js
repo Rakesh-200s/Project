2562c715a387086d9ed4602d42ee0c52277b5a3c
Rakesh Kumar <rakeshraj17082004@gmail.com>
2026-02-26 12:21:47 +0530

    initial change

---
+++ app.js
@@ -0,0 +1,102 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate=require("ejs-mate");
+const ExpressError = require("./utils/ExpressError.js");
+const listingRouter=require("./routes/listing.js");
+const reviewRouter=require("./routes/review.js");
+const session=require("express-session");
+const flash=require("connect-flash");
+const passport=require("passport");
+const LocalStrategy=require("passport-local");
+const User=require("./models/user.js");
+const userRouter=require("./routes/user.js");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+
+//Conneting to mongoDb server and DataBase
+main()
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+async function main() {
+  await mongoose.connect(MONGO_URL);
+};
+
+
+
+
+//give path to Ejs-Mate template and Public folder
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended:true}));
+app.engine("ejs",ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
+app.use(methodOverride("_method"));
+
+
+
+//Root Route
+app.get("/", (req, res) => {
+  res.send("Hi, I am root");
+});
+
+
+const sessionOptions={
+  secret: "mysupersecretcode",
+  resave: false,
+  saveUninitialized: true,
+  cookie:{
+    expires: Date.now()+ 7*24*60*1000,
+    maxAge: 7*24*60*1000,
+    httpOnly: true,
+  },
+};
+
+app.use(session(sessionOptions));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+passport.use( new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+ 
+
+app.use( (req,res,next)=>{
+  res.locals.success=req.flash("success");
+  res.locals.error=req.flash("error");
+  res.locals.currUser=req.user;
+  next();
+});
+
+//common router of listings and reviews
+app.use("/listings",listingRouter);
+app.use("/listings/:id/reviews",reviewRouter);
+app.use("/",userRouter);
+
+// Catch-all route for any path
+app.use((req,res,next)=>{
+  next(new ExpressError(404,"Page Not Found"));
+});
+//Error Handling
+app.use((err,req,res,next)=>{
+  let{statusCode=500,message="Something went wrong!"}=err;
+ res.status(statusCode).render("error.ejs",{message});
+
+  // res.send(statusCode).send(message);
+});
+
+//Host
+app.listen(8080, () => {
+  console.log("server is listening to port 8080");
+});
---
+++ routes/listing.js
@@ -0,0 +1,70 @@
+const express=require("express");
+const router= express.Router();
+const wrapAsync=require("../utils/wrapAsync.js");
+const Listing = require("../models/listing.js");
+const{isLoggedIn, isOwner,validateListing}=require("../middleware.js");
+
+
+
+//Index Route
+router.get("/", wrapAsync( async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("listings/index.ejs",{allListings})
+}));
+
+//New Route
+router.get("/new", isLoggedIn,(req, res) => {
+  res.render("listings/new.ejs");
+});
+
+//Show Route
+router.get("/:id", wrapAsync( async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id).populate(
+    {path:"reviews",
+    populate:{path:"author",}}).populate("owner");
+  if(!listing){
+    req.flash("error","You don't have permission to Edit");
+    return res.redirect("/listings");
+  };
+  res.render("listings/show.ejs", { listing });
+}));
+
+ //Create Route
+router.post("/",isLoggedIn,validateListing, wrapAsync ( async(req, res,next) => { 
+  console.log(req.body.listing);
+   const newListing = new Listing(req.body.listing);
+   newListing.owner=req.user._id;
+  await newListing.save();
+  req.flash("success","New Listing created!");
+  res.redirect("/listings");
+ }
+));
+
+//Edit Route
+router.get("/:id/edit",isLoggedIn,isOwner,wrapAsync( async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  req.flash("success"," Listing Edited!");
+  res.render("listings/edit.ejs", { listing });
+}));
+
+
+//Update Route
+router.put("/:id",isLoggedIn,isOwner,validateListing, wrapAsync( async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  req.flash("success","Listing Updated!");
+  res.redirect(`/listings/${id}`);
+}));
+
+//Delete route
+router.delete("/:id",isLoggedIn,isOwner,wrapAsync( async(req,res)=>{
+  let{id}= req.params;
+  let deletedListing = await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
+  req.flash("success","Listing Deleted!");
+  res.redirect("/listings");
+}));
+
+module.exports=router;
